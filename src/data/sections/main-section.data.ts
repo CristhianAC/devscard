4157eabab0f76f6f15ac066cd64e0efed2192612
CristhianAc <cristhian_agamez@outlook.com>
2024-11-19 17:35:30 -0500

    Update CV with new personal information, skills, and work experience; refactor main section data for improved clarity and functionality

--- src/data/sections/main-section.data.ts
+++ src/data/sections/main-section.data.ts
@@ -1,6 +1,6 @@
 import type { MainSection } from '@/types/sections/main-section.types';
 import type { ReadonlyDeep } from 'type-fest';
-import { facebook, github, linkedin, twitter } from '../helpers/links';
+import { github, linkedin } from '../helpers/links';
 
 const mainSectionData = {
   config: {
@@ -22,17 +22,16 @@ const mainSectionData = {
     { label: 'Email', value: 'cristhian_agamez@outlook.com' },
     { label: 'LinkedIn', value: '/in/cristhianac', url: 'https://linkedin.com' },
     { label: 'GitHub', value: '/cristhianac', url: 'https://github.com' },
-    
   ],
   description:
     'I am a developer with expertise in frontend and backend development, specializing in creating video games and applications. Passionate about software development, I started programming at the age of 7. I have served as a software architect for companies like Krea and Solventech, and I am currently the CEO of UnderCodes and CTO of Olympus Analytics.',
   tags: [{ name: 'Open for freelance' }, { name: 'UnderCodes, CEO' }, { name: 'Olympus Analytics, CTO' }],
   action: {
-    label: 'See my linkedin',
-    url: 'https://www.linkedin.com/in/cristhianac/',
-    downloadedFileName: '',
+    label: 'Download CV',
+    url: '/cv.pdf',
+    downloadedFileName: 'CV-CristhianAgamez.pdf',
   },
-  links: [ github({ url: 'https://github.com/cristhianac' }), linkedin({ url: 'https://linkedin.com/in/cristhianac' })],
+  links: [github({ url: 'https://github.com/cristhianac' }), linkedin({ url: 'https://linkedin.com/in/cristhianac' })],
 } as const satisfies ReadonlyDeep<MainSection>;
 
 export default mainSectionData;
